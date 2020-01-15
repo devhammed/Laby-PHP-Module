2fa6b33c1a7c88ff54a0b2f707558b2b277f5a89
Hammed Oyedele <itz.harmid@gmail.com>
2020-01-15 16:13:04 +0100

    refactor: apply DRY principles and formatted code

--- lib/robot.php
+++ lib/robot.php
@@ -1,80 +1,92 @@
 <?php
 
-  define( 'laby_name_Void', 0 );
-  define( 'laby_name_Wall', 1 );
-  define( 'laby_name_Rock', 2 );
-  define( 'laby_name_Web', 3 );
-  define( 'laby_name_Exit', 4 );
-  define( 'laby_name_Unknown', 5 );
+  define('laby_name_Void', 0);
+  define('laby_name_Wall', 1);
+  define('laby_name_Rock', 2);
+  define('laby_name_Web', 3);
+  define('laby_name_Exit', 4);
+  define('laby_name_Unknown', 5);
 
-  function output( $s ) {
+  function output($s) {
     echo $s . PHP_EOL;
   }
 
+  function check($s, $expected) {
+    return $s === ($expected . PHP_EOL);
+  }
+
   function input() {
-    $line = fgets( STDIN );
-    if ( $line == 'quit\n' ) {
-      exit( 0 );
+    $line = fgets(STDIN);
+    
+    if (check($line, 'quit')) {
+      exit(0);
     }
+    
     return $line;
   }
 
   function laby_name_left() {
-    output( 'left' );
+    output('left');
     input();
   }
 
   function laby_name_right() {
-    output( 'right' );
+    output('right');
     input();
   }
 
   function laby_name_forward() {
-    output( 'forward' );
+    output('forward');
     input();
   }
 
   function laby_name_take() {
-    output( 'take');
+    output('take');
     input();
   }
 
   function laby_name_drop() {
-    output( 'drop' );
+    output('drop');
     input();
   }
 
   function laby_name_escape() {
-    output( 'escape' );
+    output('escape');
     input();
   }
 
   function laby_name_say( $s ) {
-    output( 'say ' . $s );
+    output('say ' . $s);
     input();
   }
 
   function laby_name_look() {
-    output( 'look' );
+    output('look');
     $ans = input();
-    if ( $ans == 'void\n' ) {
+    
+    if (check($ans, 'void')) {
       return laby_name_Void;
     }
-    if ( $ans == 'wall\n' ) {
+    
+    if (check($ans, 'wall')) {
       return laby_name_Wall;
     }
-    if ( $ans == 'rock\n' ) {
+    
+    if (check($ans, 'rock')) {
       return laby_name_Rock;
     }
-    if ( $ans == 'web\n' ) {
+    
+    if (check($ans, 'web')) {
       return laby_name_Web;
     }
-    if ( $ans == 'exit\n' ) {
+    
+    if (check($ans, 'exit')) {
       return laby_name_Exit;
     }
+    
     return laby_name_Unknown;
   }
 
-  output( 'start' );
+  output('start');
   input();
 
